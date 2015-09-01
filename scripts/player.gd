1c9ddc5417088ffebf00183aa60c3324790377f4
MartiniMoe <martini.moe@cheex.de>
2015-09-01 18:13:27 +0200

    Changed particles, fixed some instancing-stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,37 @@
 extends RigidBody2D
 
-export var color = Color(1, 0, 0)
+export var player_color = Color(1, 0, 0)
 export var player_number = 0
+
+# State
 var alive = true
-var rot_speed = 2
-var move_speed = 30
-var old_position
-var trace_timer = 0
 var painting = true
+var label_hidden = false
+var old_position
+
+# Timers
+var totaltime = 0
+var trace_timer = 0
 var gap_timer = 0
 var next_gap = 0
 var gap_size = 0
-var totaltime = 0
+var deathtimer = 0
+
+# Config
+var rot_speed = 2
+var move_speed = 30
 var viewport_boundary_offset = 8
+
+# Preloads
 var trace = preload("res://trace.scn")
 var label = preload("res://label.scn")
 
 func _ready():
 	# Initialization here
-	get_node("Sprite").set_modulate(color)
-	
-	var cr = get_node("Particles").get_color_ramp()
+	get_node("Sprite").set_modulate(player_color)
+	var cr = get_node("Particles").get_color_ramp().duplicate()
 	var crc = cr.get_colors()
-	crc.set(0, color)
+	crc.set(0, player_color)
 	cr.set_colors(crc)
 	get_node("Particles").set_color_ramp(cr)
 	
@@ -32,8 +41,15 @@ func _ready():
 	gap_size = rand_range(0.2, 0.8)
 	pass
 
-func _fixed_process(delta):	
-	if alive:
+func _fixed_process(delta):
+	if !alive && !label_hidden:
+		deathtimer += delta
+		if deathtimer > 5:
+			label_hidden = true
+			var label_node = get_node("../death_label_"+str(player_number))
+			#get_parent().remove_child(label_node)
+			label_node.queue_free()
+	elif alive:
 		trace_timer += delta
 		gap_timer += delta
 		totaltime += delta
@@ -45,7 +61,7 @@ func _fixed_process(delta):
 				var new_trace = trace.instance()
 				get_parent().add_child(new_trace)
 				new_trace.set_global_pos(old_position)
-				new_trace.get_node("Sprite").set_modulate(color)
+				new_trace.get_node("Sprite").set_modulate(player_color)
 				trace_timer = 0
 		
 		if (gap_timer > next_gap) && painting:
@@ -78,6 +94,7 @@ func die():
 	alive = false
 	get_node("Particles").set_emitting(false)
 	var new_label = label.instance()
+	new_label.set_name("death_label_"+str(player_number))
 	new_label.set_text("I survived for %.1f seconds!" % totaltime)
 	get_parent().add_child(new_label)
 	var label_pos = get_global_pos()
