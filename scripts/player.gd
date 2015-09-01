9c0619f23cb68385221e5477187b4054304cabb4
MartiniMoe <martini.moe@cheex.de>
2015-09-01 11:39:21 +0200

    Added collision, death, a background and an icon

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,40 +1,45 @@
 
-extends KinematicBody2D
+extends RigidBody2D
 
 export var color = Color(1, 0, 0)
 export var player_number = 0
+var alive = true
 var rot_speed = 2
-var move_speed = 50
-var positions = []
+var move_speed = 30
+var old_position
 var timer = 0
+var totaltime = 0
+var trace = preload("res://trace.scn")
 
 func _ready():
 	# Initialization here
 	get_node("Sprite").set_modulate(color)
+	old_position = get_global_pos()
 	set_fixed_process(true)
 	pass
 
 func _fixed_process(delta):
-	var trace_tex = preload("res://res/trace.png")
 	timer += delta
+	totaltime += delta
 	
-	if timer > 0.03:
-		var new_trace = Sprite.new()
-		get_parent().add_child(new_trace)
-		new_trace.set_global_pos(get_global_pos())
-		new_trace.set_scale(Vector2(0.2, 0.2))
-		new_trace.set_texture(trace_tex)
-		new_trace.set_modulate(color)
-		timer = 0
-	
-	var move1 = Vector2(move_speed*delta, move_speed*delta)
-	var move2 = move1.rotated(get_rot())
-	move(move2)
-	
-	#if (is_colliding()):
-#		print("Collision!")
-	
-	if (Input.is_action_pressed("left_"+str(player_number))):
-		set_rot(get_rot()+(rot_speed*delta))
-	elif (Input.is_action_pressed("right_"+str(player_number))):
-		set_rot(get_rot()-(rot_speed*delta))
+	if alive:
+		if timer > 0.05:
+			var new_trace = trace.instance()
+			get_parent().add_child(new_trace)
+			new_trace.set_global_pos(old_position)
+			new_trace.get_node("Sprite").set_modulate(color)
+			timer = 0
+			old_position = get_global_pos()
+		
+		var move1 = Vector2(move_speed*delta, move_speed*delta)
+		var move2 = move1.rotated(get_rot())
+		set_pos(get_pos()+move2)
+		
+		if (totaltime > 1):
+			for body in get_colliding_bodies():
+				alive = false
+		
+		if (Input.is_action_pressed("left_"+str(player_number))):
+			set_rot(get_rot()+(rot_speed*delta))
+		elif (Input.is_action_pressed("right_"+str(player_number))):
+			set_rot(get_rot()-(rot_speed*delta))
--- scripts/world.gd
+++ scripts/world.gd
@@ -9,12 +9,17 @@ func _ready():
 	# Initialization here
 	var viewport_width = get_viewport_rect().end.x
 	var viewport_height = get_viewport_rect().end.y
+	
+	#get_node("Background").get_texture().set_size_override(Vector2(viewport_width, viewport_height))
+	#get_node("Background").set_z(-1)
+	
 	for player in get_children():
 		randomize()
 		print(str(player.get_type()))
-		if player.get_type() == "KinematicBody2D":
+		if player.get_type() == "RigidBody2D":
 			player.set_global_pos(Vector2(rand_range(20, viewport_width - 20), rand_range(20, viewport_height - 20)))
 			player.set_rot(rand_range(-3.1415, 3.1415))
+	
 	pass
 
 
